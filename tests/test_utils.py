6f20636a4c2d1dfab2c891658bac982587b024e1
Stefan Badelt <badelt@caltech.edu>
2016-11-23 17:50:49 -0800

    adapted unittests to interface changes

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,52 +3,55 @@
 import sys
 import unittest
 import subprocess as sub
-import ribolands.utils as ru
 
-class Test_vienna_stdin(unittest.TestCase):
-  """Standard fasta-like input parser that takes single name/sequence input."""
+import ribolands.utils as rutils
 
+class vienna_stdin_test(unittest.TestCase):
+  #fasta-like input parser that takes single name/sequence input
   def setUp(self):
-    self.regular_input = [
-        '>testseq with additional info in header',
-        'CUUGUUCGAUGUUGUCUUUCACGAAUCCGGUCCGCAACCCAUUAUAAGCAGAUGUGUAG']
-    self.weird_input  = [
-        '>testseq with additional info in header',
-        'CUUGUUCGAUGU ',
-        'UGUCUUUCACGAAUCCGGUCC',
-        ' GCAACCCAUUAUAAGCAGAUGUGUAG', 
-        '']
-    self.cofold_input = [
-        '>testseq with additional info in header',
-        'CUUGUUCGAUGU& ',
-        'UGUCUUUCACGAAUCCGGUCC&GCAACCCAUUAUAAGCAGAUGUGUAG']
+    pass
 
   def test_vienna_stdin(self):
-    E_result = ('testseq', 'CUUGUUCGAUGUUGUCUUUCACGAAUCCGGUCCGCAACCCAUUAUAAGCAGAUGUGUAG')
-    E_result_cofold = ('testseq', 'CUUGUUCGAUGU&UGUCUUUCACGAAUCCGGUCC&GCAACCCAUUAUAAGCAGAUGUGUAG')
-    C_result = ru.parse_vienna_stdin(self.regular_input)
-    C_result2 = ru.parse_vienna_stdin(self.weird_input)
-    C_result_cofold = ru.parse_vienna_stdin(self.cofold_input)
+    # single sequence input
+    regular_input = ['>testseq with additional info in header',
+                     'CUUGUUCGAUGUUGUCUUUCACGAAUCCGGUCCGCAACCCAUUAUAAGCAGAUGUGUAG']
 
-    self.assertEqual(E_result, C_result, 'Vienna-type fasta parser')
-    self.assertEqual(E_result, C_result2, 'Vienna-type fasta parser')
-    self.assertEqual(E_result_cofold, C_result_cofold, 'Vienna-type fasta parser')
+    fasta_input  = ['>testseq with additional info in header',
+                    'CUUGUUCGAUGU ',
+                    'UGUCUUUCACGAAUCCGGUCC',
+                    ' GCAACCCAUUAUAAGCAGAUGUGUAG', 
+                    '']
+    result = ('testseq', 'CUUGUUCGAUGUUGUCUUUCACGAAUCCGGUCCGCAACCCAUUAUAAGCAGAUGUGUAG')
+    self.assertEqual(rutils.parse_vienna_stdin(regular_input), result)
+    self.assertEqual(rutils.parse_vienna_stdin(fasta_input), result)
 
-class Test_make_pair_table(unittest.TestCase):
-  """Make a pair table representation of secondary structures."""
+  def test_fasta_stdin_(self):
+    # cofold input with multiple cutpoints
+    multifold_input = ['>testseq with additional info in header',
+                    'CUUGUUCGAUGU& ',
+                    'UGUCUUUCACGAAUCCGGUCC&GCAACCCAUUAUAAGCAGAUGUGUAG']
 
+    result = ('testseq', 'CUUGUUCGAUGU&UGUCUUUCACGAAUCCGGUCC&GCAACCCAUUAUAAGCAGAUGUGUAG')
+    self.assertEqual(rutils.parse_vienna_stdin(multifold_input), result)
+
+class make_pair_table_test(unittest.TestCase):
+  #Make a pair table representation of secondary structures.
   def setUp(self):
-    self.s1 = '(((...)).((...)).((...)))'
-    self.s2 = '(((...)).((&)).((...)))'
+    pass
 
   def test_pair_table(self):
+    s1 = '(((...)).((...)).((...)))'
     Exp_pt1_s0 = [24, 7, 6, -1, -1, -1, 2, 1, -1, 15, 14, -1, -1, -1, 10, 9, -1, 23, 22, -1, -1, -1, 18, 17, 0]
     Exp_pt1_s1 = [25, 25, 8, 7, 0, 0, 0, 3, 2, 0, 16, 15, 0, 0, 0, 11, 10, 0, 24, 23, 0, 0, 0, 19, 18, 1]
-    Cmp_pt1_s0 = ru.make_pair_table(self.s1)
-    Cmp_pt1_s1 = ru.make_pair_table(self.s1, base=1)
+    Cmp_pt1_s0 = rutils.make_pair_table(s1)
+    Cmp_pt1_s1 = rutils.make_pair_table(s1, base=1)
 
     self.assertEqual(Cmp_pt1_s0, Exp_pt1_s0, '0 based pairtable')
     self.assertEqual(Cmp_pt1_s1, Exp_pt1_s1, '1 based pairtable (C-style)')
+
+  def test_cofold_pair_table(self):
+    s2 = '(((...)).((&)).((...)))'
+    pass
 
 if __name__ == '__main__':
   unittest.main()
