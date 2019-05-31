4a41bdcc890782435d6a044346ddb8cc1f4381a4
Stefan Badelt <badelt@caltech.edu>
2019-05-31 15:43:31 -0700

    style edits

--- scripts/findpath.py
+++ scripts/findpath.py
@@ -9,29 +9,25 @@ import RNA
 def costruct(seq, cut = None):
     """ Translate between 'input' and 'internal' RNAcofold sequences """
     delim = '&'
-    if delim in seq :
+    if delim in seq:
         cut = seq.index(delim) + 1
         table = str.maketrans(dict.fromkeys('&'))
         seq = seq.translate(table)
-    elif cut and cut != -1 :
-        seq = seq[:cut-1]+delim+seq[cut-1:]
+    elif cut and cut != -1:
+        seq = seq[:cut-1] + delim + seq[cut-1:]
         cut = None
     return seq, cut
 
-def findpath_wrap(fc, s1, s2, maxE, fpath, cut = None, verbose=False):
-    """A wrapper for ViennaRNA findpath functions.
-
-    Args:
-        fc (RNA.fold_compound()): 
-    
-    """
+def findpath_wrap(fc, s1, s2, maxE, fpath, cut = None, verbose = False):
+    """ A wrapper for ViennaRNA findpath functions. """
     sE = None
-    if verbose :
+    if verbose:
         if maxE:
             dcal_bound = int(round(maxE * 100))
-            path = fc.path_findpath(s1, s2, maxE=dcal_bound, width=fpath)
+            path = fc.path_findpath(s1, s2, 
+                    maxE = dcal_bound, width = fpath)
         else:
-            path = fc.path_findpath(s1, s2, width=fpath)
+            path = fc.path_findpath(s1, s2, width = fpath)
 
         if len(path):
             sE = round(fc.eval_structure(s1), 2)
@@ -42,22 +38,23 @@ def findpath_wrap(fc, s1, s2, maxE, fpath, cut = None, verbose=False):
     else :
         if maxE:
             dcal_bound = int(round(maxE * 100))
-            dcal_sE = fc.path_findpath_saddle(s1, s2, maxE=dcal_bound, width=fpath)
+            dcal_sE = fc.path_findpath_saddle(s1, s2, 
+                    maxE = dcal_bound, width = fpath)
         else :
-            dcal_sE = fc.path_findpath_saddle(s1, s2, width=fpath)
+            dcal_sE = fc.path_findpath_saddle(s1, s2, width = fpath)
         if dcal_sE is not None:
             sE = float(dcal_sE)/100
     return sE
 
 def main():
-    """A wrapper for the ViennaRNA findpath routine."""
+    """ A wrapper for ViennaRNA findpath functions. """
     parser = argparse.ArgumentParser()
-    parser.add_argument("-v","--verbose", action="store_true",
-            help="Verbose output: print the folding pathway.")
-    parser.add_argument("-w","--search-width", type=int, default=100,
-            help="Adjust upper bound for findpath search.")
-    parser.add_argument("-m","--max-energy", type=float, default=None,
-            help="Specify upper bound for barrier energy (kcal/mol).")
+    parser.add_argument("-v","--verbose", action = "store_true",
+            help = "Verbose output: print the folding pathway.")
+    parser.add_argument("-w","--search-width", type = int, default = 100,
+            help = "Adjust upper bound for findpath search.")
+    parser.add_argument("-m","--max-energy", type = float, default = None,
+            help = "Specify upper bound for barrier energy (kcal/mol).")
     args = parser.parse_args()
 
     for e, line in enumerate(sys.stdin):
@@ -76,17 +73,17 @@ def main():
     if args.verbose:
         print("   {}".format(seq))
 
-    saddle = findpath_wrap(fc, ss1, ss2, args.max_energy, 
-            args.search_width, cut, args.verbose)
+    saddle = findpath_wrap(fc, ss1, ss2, args.max_energy, args.search_width, 
+            cut = cut, verbose = args.verbose)
 
     if saddle is not None:
         e1 = round(fc.eval_structure(ss1), 2)
-        barrier = saddle-e1
+        barrier = saddle - e1
         print("Saddle: {:6.2f} kcal/mol | Barrier: {:6.2f} kcal/mol | Search width paramter: {}".format(saddle, barrier, args.search_width))
     else:
         print("No valid saddle found below energy: {:6.2f} kcal/mol | Search width parameter: {}".format(args.max_energy, args.search_width))
 
 
 if __name__ == '__main__':
-  main()
+    main()
 
